de76f42b6123ce1d07cd219df8c5d5298a327277
Mondal <subham.a.mondal@capgemini.com>
2023-12-20 20:19:34 +0530

    adding the practise files

---
+++ python3.py
@@ -0,0 +1,7 @@
+number = 70
+
+# Check the is more than 70 or not
+if (number >= 70):
+    print("You have passed")
+else:
+    print("You have not passed")
---
+++ python8.py
@@ -0,0 +1,20 @@
+fruits = ["Mango", "Orange", "Guava", "Banana"]
+
+# Insert an item in the 2nd position
+fruits.insert(1, "Grape")
+
+# Displaying list after inserting
+print("The fruit list after insert:")
+print(fruits)
+
+# Remove an item
+fruits.remove("Guava")
+
+# Print the list after delete
+print("The fruit list after delete:")
+print(fruits)
+
+#print list after addition
+print('fruits after addition:')
+fruits.append("strawberry")
+print(fruits)
